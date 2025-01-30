e1af8d6ec930eb91704dcad11fe5c0547b34f466
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2025-01-30 10:26:41 +0100

    delete & get by id

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ app.get("/people", (req, res) => {
   res.send(data);
 });
 
+app.get("/people/:id", (req, res) => {
+  let id = req.params.id;
+  res.send(data[id]);
+});
+
+app.delete("/people/:id", (req, res) => {
+  let id = req.params.id;
+  data.splice(id, 1);
+  res.send("deleted");
+});
+
 app.post("/people", (req, res) => {
   data.push(req.body);
 
